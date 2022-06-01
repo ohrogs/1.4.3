19aa6b437e5fb1365d16ed07e0240f83e82572d9
ohrogs <lorenzo.gr.mari@gmail.com>
2022-06-01 15:47:46 +0200

    commmit

---
+++ b/console1/PrimaConsole/PrimaConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Area rettangolo 3,4 = " + AreaRettangolo(3.0,4.0));
+            Console.WriteLine("Area rettangolo 3,4 = {0}", AreaRettangolo(3.0, 4.0)); //lo 0 indica la posizione dei parametri dopo la ,
+            Console.ReadKey();
+        }
+
+        static double AreaRettangolo(double b, double h)
+        {
+            return b * h;
+        }
+    }
+}
