12d86ec98d80f54d5cde06304c761c50b08297e3
ohrogs <lorenzo.gr.mari@gmail.com>
2022-06-06 08:59:16 +0200

    es venerdi

---
+++ b/ConsoleArgomenti/ConsoleArgomenti/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArgomenti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+        }
+    }
+}
---
+++ b/ConsoleLinguaggio/ConsoleLinguaggio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinguaggio
+{
+    internal class Program
+    {
+        struct punto
+        {
+            public double x;
+            public double y;
+        }
+        enum Giorni
+        {
+            Lunedi,Martedi,Mercoledi,Giovedi,Venerdi,Sabato,Domenica
+        }
+        static void Main(string[] args)
+        {
+            punto p;
+            Giorni a = Giorni.Giovedi;
+            p.x = 1.0;
+            p.y = 2.0;
+            Console.WriteLine("(" + p.x + ", " + p.y + ")");
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
