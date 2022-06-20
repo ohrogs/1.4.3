0cbdaae24fa7796876b69b11d4c811316cd214a6
ohrogs <lorenzo.gr.mari@gmail.com>
2022-06-20 16:05:51 +0200

    Primo cmmit

---
+++ b/20-06-2022/STRUKT/STRUKT/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRUKT
+{
+    internal class Program
+    {
+        struct punto
+        {
+            public int x;
+            public int y;
+
+            public void Stampa()
+            {
+                Console.WriteLine("(" + x + "; " + y + ")");
+            }
+        }
+
+        enum Giorni
+        {
+            Lunedi = 1,
+            Martedi,
+            Mercoledi,
+            Giovedi,
+            Venerdi,
+            Sabato,
+            Domenica //Sará 7
+        }
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Title = "SIUUUUU";
+            Conversioni();
+            punto punto; //esiste e occupa gia lo spazio
+            punto.x = 1;
+            punto.y = 2;
+            punto.Stampa();
+            Console.WriteLine(Giorni.Lunedi + " = " + (int)Giorni.Lunedi);
+            Console.ReadKey();
+        }
+
+        static void Conversioni()
+        {
+            long l = 1234567;
+
+            int i = (int)l;
+
+            int k = (int)l;
+
+            byte b = (byte)i;
+        }
+    }
+}
--- b/ConsoleArgomenti/ConsoleArgomenti/Program.cs
+++ b/ConsoleArgomenti/ConsoleArgomenti/Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleArgomenti
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
+            //Console.WriteLine("SIUM");
+            Console.WriteLine("il tuo nome é " + args[0]);
+            String nome = Console.ReadLine();
+            Console.WriteLine(AreaRettangolo(5,7) + " SDROGO");
+
+
+            Console.ReadKey();//per concludere il programma
+            
+        }
+        private static int AreaRettangolo(int lato1, int lato2)
+        {
+            return lato1 * lato2;
         }
     }
 }
