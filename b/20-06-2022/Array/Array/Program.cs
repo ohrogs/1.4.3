1b8c2464a1f23bdd203f0126f34aee51f417dce0
ohrogs <lorenzo.gr.mari@gmail.com>
2022-06-21 12:06:13 +0200

    altro csharp

---
+++ b/20-06-2022/Array/Array/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vettore();
+            matrice();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ReadKey();
+        }
+
+        private static void vettore()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            int[] vettore; //é un riferimento come la stringa
+            vettore = new int[5];
+
+
+            for(int i = 0; i < vettore.Length; i++)
+            {
+                vettore[i] = (i+1)*10;
+                Console.Write(vettore[i] +" ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void matrice()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            int[,] matrice = new int[3,5];
+            //int [,] il numero di colonne é fisso x ogni riga
+            //int [][] é un array di array, ogni riga puo avere un numero di colonne diverso
+
+            RiempiMatrice(matrice);
+            StampaMatrice(matrice);
+
+            
+        }
+
+        static void RiempiMatrice(int[,] mat)
+        {
+            for(int i = 0; i < mat.GetLength(0); i++)//getlenght restituisce la lun della prima dim della stringa
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    mat[i,j] = (i+2)*(j+2)*10;
+                }
+            }
+        }
+        static void StampaMatrice(int[,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)//getlenght restituisce la lun della prima dim della stringa
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write(mat[i,j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
--- b/20-06-2022/STRUKT/STRUKT/Program.cs
+++ b/20-06-2022/STRUKT/STRUKT/Program.cs
@@ -31,9 +31,17 @@ namespace STRUKT
         }
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Red;
             Console.Title = "SIUUUUU";
             Conversioni();
+            tryparse();
+            opternario();
+            String s1 = "Vamo raga";
+            //Sulla stack cé il punatore s1 che punta a vamo raga sulla heap
+            String s2 = s1;
+            //Creo un nuovo puntatore e ci metto dentro l'indirizzo di vamo raga nella heap
+            s1 = "altro";
+            //Vamo raga non si é cancellato
+            //ora s1 punta ad altro mentre s2 ancora a vamo raga;
             punto punto; //esiste e occupa gia lo spazio
             punto.x = 1;
             punto.y = 2;
@@ -44,6 +52,7 @@ namespace STRUKT
 
         static void Conversioni()
         {
+            Console.BackgroundColor = ConsoleColor.Blue;
             long l = 1234567;
 
             int i = (int)l;
@@ -51,6 +60,42 @@ namespace STRUKT
             int k = (int)l;
 
             byte b = (byte)i;
+
+            Console.WriteLine(Convert.ToBoolean(l));
+            Console.WriteLine(Convert.ToString(b));
+            Console.WriteLine(Convert.ToInt32("6969"));
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+        }
+        static void tryparse()
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+
+            int numero;
+
+            if(int.TryParse("15.3", out numero))
+            {
+                Console.WriteLine("Valore convertito = " + numero);
+            }
+            else
+            {
+                Console.WriteLine("SEI UN COGLIONE");
+            }
+            
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+        }
+
+        static void opternario()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            int x = 2;
+            int y = 3;
+            int risultato = (x > y) ? x : y;
+            //Se x > y risultato = x
+            //Altrimento risultato = y
+            Console.WriteLine(risultato);
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
         }
     }
 }
