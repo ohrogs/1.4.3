ef5c33f987ea030261d325c5b545f7e3722c7ac9
ohrogs <lorenzo.gr.mari@gmail.com>
2022-06-22 12:51:18 +0200

    sium

---
+++ b/22-06-2022/Classi/Classi/Persona.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    internal class Persona
+    {
+        private int eta;
+        private string nome;
+        private string cognome;
+        private String cf;
+
+
+        //Properties
+        public String Cognome
+        {
+            get 
+            {
+                return cognome;
+            }
+            set
+            {
+                if(value.Length >= 2)
+                    cognome = value;
+            } 
+        }
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        //metodi
+        public Persona()
+        {
+            eta = 0;
+            nome = null;
+            cognome = null;
+            cf = null;
+        }
+        public Persona(int eta, string nome, string cognome, string cf)
+        {
+            this.eta = eta;
+            this.nome = nome;
+            this.cognome = cognome;
+            this.cf = cf;
+        }
+        public String GetInfo()
+        {
+            return nome + " " + cognome + ", " + eta;
+        }
+    }
+}
--- b/22-06-2022/Classi/Classi/Program.cs
+++ b/22-06-2022/Classi/Classi/Program.cs
@@ -10,6 +10,17 @@ namespace Classi
     {
         static void Main(string[] args)
         {
+            Persona persona = new Persona(18, "carlo", "brozzo");
+            Console.WriteLine(persona.GetInfo());
+            persona.Cognome = "culo";
+            Console.WriteLine(persona.GetInfo().ToString());
+            pause();
+        }
+
+        private static void pause()
+        {
+            Console.WriteLine("Premere un tasto per continuare");
+            Console.ReadKey();
         }
     }
 }
