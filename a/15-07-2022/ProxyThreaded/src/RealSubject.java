005e8d01d085d7eb880ab14d7510774484bbca62
ohrogs <lorenzo.gr.mari@gmail.com>
2022-07-16 10:36:24 +0200

    roba

---
+++ a/15-07-2022/ProxyThreaded/src/RealSubject.java
@@ -0,0 +1,12 @@
+import java.time.Instant;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RealSubject implements Subject{
+    public int getData()
+    {
+        int ret = ThreadLocalRandom.current().nextInt(0, 110);
+        System.out.println("Ore: " + Date.from(Instant.now()) + " Realsubject restituisce " + ret);
+        return ret;
+    }
+}
